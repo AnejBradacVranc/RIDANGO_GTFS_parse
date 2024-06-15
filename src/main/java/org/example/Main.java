de931403169e916195e6ed7ae54a656cdb2c6bc8
AnejBradacVranc <mikedrifner@gmail.com>
2024-06-15 17:17:00 +0200

    Added so the max time displayed is two hours from now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,17 @@ package org.example;
 import org.onebusaway.csv_entities.exceptions.CsvEntityIOException;
 import org.onebusaway.csv_entities.exceptions.MissingRequiredFieldException;
 import org.onebusaway.gtfs.impl.GtfsDaoImpl;
-import org.onebusaway.gtfs.model.*;
+import org.onebusaway.gtfs.model.Route;
+import org.onebusaway.gtfs.model.Stop;
+import org.onebusaway.gtfs.model.StopTime;
+import org.onebusaway.gtfs.model.Trip;
 import org.onebusaway.gtfs.serialization.GtfsReader;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
-import java.util.Map;
 import java.util.Objects;
 
 public class Main {
@@ -23,9 +26,8 @@ public class Main {
         GtfsDaoImpl store = new GtfsDaoImpl();
         reader.setEntityStore(store);
 
-        try
-        {
-        reader.run();
+        try {
+            reader.run();
 
 
 
@@ -61,25 +63,34 @@ public class Main {
 
             //System.out.println(stopTimeById.get(selectedStop).getTrip().getRoute().getShortName());
 
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+            LocalTime currentTime = LocalTime.now();
+            LocalTime maxTime = currentTime.plusHours(2);
 
-            for (StopTime stopTime : store.getAllStopTimes()){
+
+            Collection<StopTime> stopTimes = store.getAllStopTimes();
+
+
+            stopTimes.removeIf(p -> !Objects.equals(p.getStop().getId().getId(), "3"));
+
+            stopTimes.removeIf(p -> !LocalTime.ofSecondOfDay(p.getArrivalTime()).isAfter(currentTime) ||  !LocalTime.ofSecondOfDay(p.getArrivalTime()).isBefore(maxTime));
+
+
+            for (StopTime stopTime : stopTimes) {
+
                 Stop stop = stopTime.getStop();
                 Trip trip = stopTime.getTrip();
                 Route route = trip.getRoute();
+                LocalTime timeOfArrival = LocalTime.ofSecondOfDay(stopTime.getArrivalTime());
 
-
-                if(Objects.equals(stop.getId().getId(), "4")){
-                    System.out.println("Postajalisce " + stop.getName()+ " " + stop.getId().getId());
-                    System.out.println("Linija " + route.getShortName());
-                    System.out.println("Cas odhoda " +  stopTime.getDepartureTime());
-                    System.out.println("Cas prihoda " + stopTime.getArrivalTime());
-                }
+                System.out.println("Postajalisce " + stop.getName() + " id:" + stop.getId().getId());
+                System.out.println("Linija " + route.getShortName());
+                System.out.println("Cas prihoda " + timeOfArrival.format(formatter) + "\n");
 
             }
 
 
-
-        }catch (MissingRequiredFieldException | CsvEntityIOException e){
+        } catch (MissingRequiredFieldException | CsvEntityIOException e) {
             System.out.println(e.getMessage());
         }
 
